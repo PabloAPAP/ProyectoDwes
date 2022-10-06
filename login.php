09296d32bfc3f4326f9e8248d4e6158753b560ce
pablo <pabloap@alumnos.iesgalileo.es>
2022-10-06 19:31:00 +0200

    0

--- login.php
+++ login.php
@@ -1,12 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 </head>
+
 <body>
+    <?php
+    $usuarioRegistrado = "pepe";
+    $passwordRegistado = "1234";
+    $usuarioOk = "false";
+    $passwordOk = "false";
     
+
+    if (!empty($_POST)) {
+        $usuario = htmlspecialchars($_POST["usuario"]);
+        $password = htmlspecialchars($_POST["password"]);
+
+        if (empty($usuario)) {
+            echo "No ha introducido un nombre de usuario";
+        } else if ($usuario == $usuarioRegistrado) {
+            $usuarioOk = "true";
+        } else if ($usuario != $usuarioRegistrado) {
+            echo "error, usuario no registrado";
+        }
+
+        if (empty($password)) {
+            echo "No ha introducido una contraseña";
+        } else if ($password == $passwordRegistado) {
+            $passwordOk = "true";
+        }
+
+        if( $usuarioOk == "true" && $passwordOk == "true"){
+            echo "Bienvenido $usuario";
+        }
+    }
+    ?>
+
+
+    <form action='<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>' method="post">
+        Usuario: <input type="text" name="usuario">
+        <br>
+        Contraseña: <input type="password" name="password">
+        <br>
+        <input type="submit" value="acceso">
+    </form>
 </body>
+
 </html>
