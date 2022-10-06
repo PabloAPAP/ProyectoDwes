864dc86c767f38840eb4347506b9e76785ab6909
pablo <pabloap@alumnos.iesgalileo.es>
2022-10-06 17:39:47 +0200

    patatas

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <title>Proyecto Dwes</title>
 </head>
 <body>
-    <!-- ¡Que cojones es esto...! -->
+    <!-- ¡Que cojones es esto...!  = patatas-->
     prueba guille push
     git config --global user.email "you@example.com"
     git config --global user.name "Your Name"
