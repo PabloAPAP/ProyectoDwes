ae513783622b5f8f78540190d339a3cb95aa92d6
Laura <lacafe93@yahoo.es>
2022-10-24 14:54:39 +0200

    validacion avatar ok

--- registro.php
+++ registro.php
@@ -10,11 +10,11 @@
 </head>
 
 <body>
-    <!-- Validación del formulario -->
     <?php
+    //Validación del formulario
 
     include 'scripts/funciones.php';
-    $exito = $nombreError =  $passError = $pass2Error = $emailError = $fechaError = $imagenError = $errorVacios = "";
+    $nombreError =  $passError = $pass2Error = $emailError = $fechaError = $imagenError = $errorVacios = "";
     $nombreUsuarioOK = $passwordOK = $password2OK = $emailOK = $fechaNacOK = $imagenAvatarOK = "";
     if (!empty($_POST)) {
 
@@ -26,9 +26,7 @@
         $password2 = htmlspecialchars($_POST["password2"]);
         $email = htmlspecialchars($_POST["email"]);
         $fechaNac = htmlspecialchars($_POST["fechaNac"]);
-        $tamañoAvatar = $_FILES["imagenAvatar"]['size'];
-        $imagenAvatar = $_FILES["imagenAvatar"]['name'];
-
+        $imagenAvatar = $_FILES["imagenAvatar"];
 
         // Comprobamos que los campos no están vacíos y que validan
         if (!empty($nombreUsuario)) {
@@ -82,12 +80,21 @@
             $fechaNacOK = $fechaNac;
         }
 
-        if ($tamañoAvatar == 0) {
-            $imagenError = "Tienes que seleccionar una foto de perfil";
-            $algunError = true;
+        //Si el tamaño del fichero es mayor que 0, quiere decir que tenemos un fichero.
+        if ($imagenAvatar["size"] > 0) {
+            //Comprobamos el tipo de imagen del avatar
+            $tipo = $imagenAvatar["type"];
+            if ($tipo == "image/jpg" || $tipo == "image/jpeg" || $tipo == "image/png") {
+                $imagenAvatarOK = $imagenAvatar;
+            } else {
+                $algunError = true;
+                $imagenError = "El fichero debe ser jpeg, jpg o png";
+            }
         } else {
-            $imagenAvatar = $imagenAvatarOK;
+            $algunError = true;
+            $imagenError = "Tienes que subir una imagen de perfil";
         }
+
 
         if (empty($nombreUsuario) && empty($password) && empty($email) && empty($fechaNac) && empty($imagenAvatar)) {
             $algunError = true;
@@ -96,47 +103,50 @@
 
         //Si no hay errores entra al login.
         if (!$algunError) {
-            $exito = "Te has registrado con exito";
-            sleep(2);
-            header("Location: login.php");
+            echo"<script language='JavaScript'>alert('Registrado con exito!')</script>";
+            echo"<script language='JavaScript'>window.location = ('login.php');</script>";
+            exit();
         }
     }
     ?>
 
     <form action='<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>' method="post" enctype="multipart/form-data" class="login-form">
-        <h1>Registrarse</h1>
-        <p>Nombre de Usuario <span class="error"> *</span></p>
+        <h1>Regístrate</h1>
+
+        <p>Nombre de Usuario *</p>
         <span class="error"><?php echo $nombreError; ?></span>
         <input type="text" name="nombreUsuario" value="<?php echo $nombreUsuarioOK; ?>">
 
-        <p>Contraseña <span class="error"> * </span></p>
+        <p>Contraseña *</p>
         <span class="error"><?php echo $passError; ?></span>
         <input type="password" name="password" value="<?php echo $passwordOK; ?>">
         </span><br><br>
 
-        <p>Repite la contraseña <span class="error"> * </span></p>
+        <p>Repite la contraseña *</p>
         <span class="error"><?php echo $pass2Error; ?></span>
         <input type="password" name="password2" value="<?php echo $password2OK; ?>">
         </span><br><br>
 
-        <p>Correo electrónico <span class="error"> *</span></p>
+        <p>Correo electrónico *</p>
         <span class="error"><?php echo $emailError; ?></span>
         <input type="text" name="email" value="<?php echo $emailOK; ?>">
         <br><br>
 
-        <p> Fecha de nacimiento <span class="error"> *</span></p>
+        <p> Fecha de nacimiento *</p>
         <span class="error"><?php echo $fechaError; ?></span>
         <input type="date" name="fechaNac" value="<?php echo $fechaNacOK; ?>">
         <br><br>
 
-        <p>Imagen de perfil <span class="error"> *</span></p>
+        <p>Imagen de perfil *</p>
         <span class="error"><?php echo $imagenError; ?></span>
         <input type="file" id="avatar" name="imagenAvatar" accept="image/png, image/gif, image/jpeg" value="<?php echo $imagenAvatarOK; ?>" />
 
         <br><br>
+        <h5>Los campos marcados con * son obligatorios</h5>
         <input type="submit" value="Registrarse" name="submit" />
         <span class="error"><?php echo $errorVacios; ?></span>
-        <span><?php echo $exito; ?></span>
+        <p><a href='login.php'>¿Ya tienes cuenta? Logueate</a></p>
+
         <br><br>
 
     </form>
