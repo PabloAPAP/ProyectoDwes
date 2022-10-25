65568140f4d33ccbd38bf614b9829fe80f6003eb
davidrpgalileo <davidrp@alumnos.iesgalileo.es>
2022-10-25 17:21:57 +0200

    Multiidioma

---
+++ lang/en.php
@@ -0,0 +1,12 @@
+<?php
+$lang = array(
+    "titulo" => "Home",
+    "login" => "Login",
+    "registro" => "Sign In",
+    "esp" => "Spanish",
+    "eng" => "English",
+    "ini" => "Inicio de Sesión",
+    "nomUsu" => "Nombre de Usuario",
+    "password"=>"Contraseña"
+)
+?>
---
+++ lang/es.php
@@ -0,0 +1,10 @@
+<?php
+$lang['titulo'] = "Inicio";
+$lang['login'] = "Iniciar Sesión";
+$lang['registro'] = "Registrarse";
+$lang['esp'] = "Español";
+$lang['eng'] ="Inglés";
+$lang['ini'] ="Inicio de Sesión";
+$lang['nomUsu'] ="Nombre de Usuario";
+$lang['password'] ="Contraseña";
+?>
