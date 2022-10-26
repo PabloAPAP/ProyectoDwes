a8dafab1ed90631944a6199caaba3548835f7a47
Laura <lacasito93@yahoo.es>
2022-10-26 20:26:04 +0200

    orgia lesbica

--- login.php
+++ login.php
@@ -40,6 +40,7 @@
         if (empty($password)) {
             $_password_err = "No ha introducido una contraseña";
         } else {
+            echo md5($password, PASSWORD_DEFAULT);
             if ($usuario === $usuarioBBDD && md5($password, PASSWORD_DEFAULT) === $passBBDD) {
                 //propago sesion
             //session_start();
@@ -47,13 +48,15 @@
             $_SESSION['usuario'] = $usuario;
             $_SESSION['pass']=$password;
             //$_SESSION['equipo'] = $_SERVER['REMOTE_ADDR']; 
+            }else{
+                $_password_err = "Contraseña incorrecta";
             }
         }
     }
     //Mostramos el nombre de usuario y la opción de cerrar sesión
     if (isset($_SESSION['usuario'])) {
         echo "<p>Usuario: " . $_SESSION['usuario'] . "</p>";
-        echo "<a href=logout.php>Cerrar Sesion</a>";
+        echo "<a href=cerrarSesion.php>Cerrar Sesion</a>";
     } else {
         //Muestro el formulario de inicio
     ?>
--- registro.php
+++ registro.php
@@ -30,24 +30,17 @@
             $imagenAvatar = $_FILES["imagenAvatar"];
 
             // Comprobamos que los campos no están vacíos y que validan
-            if (empty($nombreUsuario)) {
+            if (!empty($nombreUsuario)) {
                 //si buscamos al usuario y devuleve un numero quiere decir que existe
                 if (buscarUsuario($nombreUsuario) !== false) {
-                }
-                if (!validar($nombreUsuario, VALIDA_USUARIO)) {
-                    $nombreError = "El campo 'Nombre de Usuario' es incorrecto";
-                    if (buscarUsuario($nombreUsuario) !== false) { //Ya existe alguien registrado con ese nombre
-                        $nombreError = "Ya existe un usuario con ese nombre";
+                    $nombreError = "Ya existe un usuario con ese nombre";
+                    $algunError = true;
+                } else { //No existe ningun usuario registrado con ese nombre, por lo tanto validamos el usuario
+                    if (!validar($nombreUsuario, VALIDA_USUARIO)) {
+                        $nombreError = "El campo 'Nombre de Usuario' es incorrecto";
                         $algunError = true;
-                    } else { //No existe ningun usuario registrado con ese nombre, por lo tanto validamos el usuario
-                        if (!validar($nombreUsuario, VALIDA_USUARIO)) {
-                            $nombreError = "El campo 'Nombre de Usuario' es incorrecto";
-                            $algunError = true;
-                        } else {
-
-
-                            $nombreUsuarioOK = $nombreUsuario;
-                        }
+                    } else {
+                        $nombreUsuarioOK = $nombreUsuario;
                     }
                 }
             } else {
@@ -117,71 +110,137 @@
 
             //Si no hay errores entra al login y mete el usuario y la contraseña en el registro.
             if (!$algunError) {
+
                 $passCifrado  = md5($password, PASSWORD_DEFAULT);
                 $ficheroContrasenas = fopen("acceso/usuariosPassword.txt", "a");
                 fwrite($ficheroContrasenas, "$nombreUsuario|$passCifrado" . PHP_EOL);
                 fclose($ficheroContrasenas);
-                header("Location: login.php");
+        ?> <h1>Registado con exito</h1>
+                <button class="login-form"><a href="login.php">Login</a></button>
+
+            <?php
+            } else { ?>
+                <form action='<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>' method="post" enctype="multipart/form-data" class="login-form">
+                    <h1>Regístrate</h1>
+
+                    <p>Nombre de Usuario *</p>
+                    <span class="error"><?php echo $nombreError; ?></span>
+                    <input type="text" name="nombreUsuario" value="<?php echo $nombreUsuarioOK; ?>">
+
+                    <p>Contraseña *</p>
+                    <span class="error"><?php echo $passError; ?></span>
+                    <div class="btnAlinear">
+                        <input type="password" class="form-control mb-0" id="password1" name="password1" value="<?php echo $passwordOK; ?>">
+                        <button id="show_password" class="btnMostrar" type="button" onclick="mostrarPassword()"> <span class="fa fa-eye-slash icon"></span> </button>
+                    </div>
+
+                    <p>Repite la contraseña *</p>
+                    <span class="error"><?php echo $pass2Error; ?></span>
+                    <div class="btnAlinear">
+                        <input type="password" class="form-control mb-0" id="password2" name="password2" value="<?php echo $password2OK; ?>">
+                        <button id="show_password" class="btnMostrar" type="button" onclick="mostrarPassword()"> <span class="fa fa-eye-slash icon"></span> </button>
+                    </div>
+
+                    <p>Correo electrónico *</p>
+                    <span class="error"><?php echo $emailError; ?></span>
+                    <input type="text" name="email" value="<?php echo $emailOK; ?>">
+
+                    <p> Fecha de nacimiento *</p>
+                    <span class="error"><?php echo $fechaError; ?></span>
+                    <input type="date" name="fechaNac" value="<?php echo $fechaNacOK; ?>">
+
+                    <p>Imagen de perfil *</p>
+                    <span class="error"><?php echo $imagenError; ?></span>
+                    <input type="file" id="avatar" name="imagenAvatar" accept="image/png, image/gif, image/jpeg" value="<?php echo $imagenAvatarOK; ?>" />
+
+                    <h6>Los campos marcados con * son obligatorios</h6>
+                    <input type="submit" value="Registrarse" name="submit" />
+                    <span class="error"><?php echo $errorVacios; ?></span>
+                    <p style="text-decoration-line: underline ;"><a href='login.php'>¿Ya tienes cuenta? Logueate</a></p>
+                </form>
+
+                <script type="text/javascript">
+                    function mostrarPassword() {
+                        var cambio = document.getElementById("password1");
+                        var cambio2 = document.getElementById("password2");
+                        if (cambio.type == "password") {
+                            cambio.type = "text";
+                            cambio2.type = "text";
+                            $('.icon').removeClass('fa fa-eye-slash').addClass('fa fa-eye');
+                        } else {
+                            cambio.type = "password";
+                            cambio2.type = "password";
+                            $('.icon').removeClass('fa fa-eye').addClass('fa fa-eye-slash');
+                        }
+                    }
+                </script>
+            <?php
             }
-        }
+        } else {
 
-        ?>
 
-        <form action='<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>' method="post" enctype="multipart/form-data" class="login-form">
-            <h1>Regístrate</h1>
 
-            <p>Nombre de Usuario *</p>
-            <span class="error"><?php echo $nombreError; ?></span>
-            <input type="text" name="nombreUsuario" value="<?php echo $nombreUsuarioOK; ?>">
+            ?>
 
-            <p>Contraseña *</p>
-            <span class="error"><?php echo $passError; ?></span>
-            <div class="btnAlinear">
-                <input type="password" class="form-control mb-0" id="password1" name="password1" value="<?php echo $passwordOK; ?>">
-                <button id="show_password" class="btnMostrar" type="button" onclick="mostrarPassword()"> <span class="fa fa-eye-slash icon"></span> </button>
-            </div>
+            <form action='<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>' method="post" enctype="multipart/form-data" class="login-form">
+                <h1>Regístrate</h1>
 
-            <p>Repite la contraseña *</p>
-            <span class="error"><?php echo $pass2Error; ?></span>
-            <div class="btnAlinear">
-                <input type="password" class="form-control mb-0" id="password2" name="password2" value="<?php echo $password2OK; ?>">
-                <button id="show_password" class="btnMostrar" type="button" onclick="mostrarPassword()"> <span class="fa fa-eye-slash icon"></span> </button>
-            </div>
+                <p>Nombre de Usuario *</p>
+                <span class="error"><?php echo $nombreError; ?></span>
+                <input type="text" name="nombreUsuario" value="<?php echo $nombreUsuarioOK; ?>">
 
-            <p>Correo electrónico *</p>
-            <span class="error"><?php echo $emailError; ?></span>
-            <input type="text" name="email" value="<?php echo $emailOK; ?>">
+                <p>Contraseña *</p>
+                <span class="error"><?php echo $passError; ?></span>
+                <div class="btnAlinear">
+                    <input type="password" class="form-control mb-0" id="password1" name="password1" value="<?php echo $passwordOK; ?>">
+                    <button id="show_password" class="btnMostrar" type="button" onclick="mostrarPassword()"> <span class="fa fa-eye-slash icon"></span> </button>
+                </div>
 
-            <p> Fecha de nacimiento *</p>
-            <span class="error"><?php echo $fechaError; ?></span>
-            <input type="date" name="fechaNac" value="<?php echo $fechaNacOK; ?>">
+                <p>Repite la contraseña *</p>
+                <span class="error"><?php echo $pass2Error; ?></span>
+                <div class="btnAlinear">
+                    <input type="password" class="form-control mb-0" id="password2" name="password2" value="<?php echo $password2OK; ?>">
+                    <button id="show_password" class="btnMostrar" type="button" onclick="mostrarPassword()"> <span class="fa fa-eye-slash icon"></span> </button>
+                </div>
 
-            <p>Imagen de perfil *</p>
-            <span class="error"><?php echo $imagenError; ?></span>
-            <input type="file" id="avatar" name="imagenAvatar" accept="image/png, image/gif, image/jpeg" value="<?php echo $imagenAvatarOK; ?>" />
+                <p>Correo electrónico *</p>
+                <span class="error"><?php echo $emailError; ?></span>
+                <input type="text" name="email" value="<?php echo $emailOK; ?>">
 
-            <h6>Los campos marcados con * son obligatorios</h6>
-            <input type="submit" value="Registrarse" name="submit" />
-            <span class="error"><?php echo $errorVacios; ?></span>
-            <p style="text-decoration-line: underline ;"><a href='login.php'>¿Ya tienes cuenta? Logueate</a></p>
-        </form>
+                <p> Fecha de nacimiento *</p>
+                <span class="error"><?php echo $fechaError; ?></span>
+                <input type="date" name="fechaNac" value="<?php echo $fechaNacOK; ?>">
 
-        <script type="text/javascript">
-            function mostrarPassword() {
-                var cambio = document.getElementById("password1");
-                var cambio2 = document.getElementById("password2");
-                if (cambio.type == "password") {
-                    cambio.type = "text";
-                    cambio2.type = "text";
-                    $('.icon').removeClass('fa fa-eye-slash').addClass('fa fa-eye');
-                } else {
-                    cambio.type = "password";
-                    cambio2.type = "password";
-                    $('.icon').removeClass('fa fa-eye').addClass('fa fa-eye-slash');
+                <p>Imagen de perfil *</p>
+                <span class="error"><?php echo $imagenError; ?></span>
+                <input type="file" id="avatar" name="imagenAvatar" accept="image/png, image/gif, image/jpeg" value="<?php echo $imagenAvatarOK; ?>" />
+
+                <h6>Los campos marcados con * son obligatorios</h6>
+                <input type="submit" value="Registrarse" name="submit" />
+                <span class="error"><?php echo $errorVacios; ?></span>
+                <p style="text-decoration-line: underline ;"><a href='login.php'>¿Ya tienes cuenta? Logueate</a></p>
+            </form>
+
+            <script type="text/javascript">
+                function mostrarPassword() {
+                    var cambio = document.getElementById("password1");
+                    var cambio2 = document.getElementById("password2");
+                    if (cambio.type == "password") {
+                        cambio.type = "text";
+                        cambio2.type = "text";
+                        $('.icon').removeClass('fa fa-eye-slash').addClass('fa fa-eye');
+                    } else {
+                        cambio.type = "password";
+                        cambio2.type = "password";
+                        $('.icon').removeClass('fa fa-eye').addClass('fa fa-eye-slash');
+                    }
                 }
+            </script>
             }
-        </script>
 
+        <?php
+        }
+        ?>
     </body>
 
     </html>
