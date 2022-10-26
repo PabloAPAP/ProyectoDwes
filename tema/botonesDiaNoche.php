d0e38dc5a390c132dfb4f5d72834576adeb7811b
pablo <pabloap@alumnos.iesgalileo.es>
2022-10-26 20:56:45 +0200

    getChorizoPortuano();

--- tema/botonesDiaNoche.php
+++
@@ -1,6 +0,0 @@
-<link rel="stylesheet" href="style.css">
-
-<a href="funcioncookie.php?usertheme=style"><img src="Anochecer32.ico"></a>
-
-
-<a href="funcioncookie.php?usertheme=style"><img src="Amanecer.png"></a>
